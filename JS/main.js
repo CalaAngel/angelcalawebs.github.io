68c9c4724d0fe8dca4e8957b45bc0d7641b2ad85
CalaAngel <kalaengel99@gmail.com>
2021-04-11 20:04:46 -0500

    v1.0

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+AOS.init();
+let ubicPrin=window.pageYOffset;
+
+window.addEventListener("scroll", function(){
+    let despAct= window.pageYOffset;
+    if(ubicPrin>=despAct){
+        document.getElementsByTagName("nav")[0].style.top="0px"
+    }else{
+        document.getElementsByTagName("nav")[0].style.top="-100px"
+    }
+    ubicPrin=despAct;
+})
+//RESPONSIVE NAVBAR
+let HeaderEnlac=document.querySelectorAll(".enlaces-header")[0];
+let temp=true;
+
+document.querySelectorAll(".barras")[0].addEventListener("click", function(){
+    HeaderEnlac.classList.toggle("menu2")
+})
